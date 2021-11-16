b09b355c5dd7a3dbd4465afa11192e5aff93d4cc
Isuru Pathum Herath <isurupathumherath@gmail.com>
2021-11-16 19:33:14 +0530

    Added 404 Not Found Page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div class="container-fluid">
+            <div class="text-center" style={{ marginTop: "150px" }}>
+                <div class="error mx-auto" data-text="404">404</div>
+                <p class="lead text-gray-800 mb-5">Page Not Found</p>
+                <p class="text-gray-500 mb-0">It looks like Page is not Found</p>
+                <a href="/">&larr; Back to Dashboard</a>
+            </div>
+
+        </div>
+    )
+}
+
+export default NotFound
--- src/router.js
+++ src/router.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import PrivateRoute from './api/privateRoutes';
 
 import Login from './components/login/login';
 import App from './App';
+import NotFound from './pages/NotFound';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
+                <Route path='/404' component={NotFound} />
                 <PrivateRoute path="/" exact component={App} />
                 <Route path="/login" exact component={Login} />
+                <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
     );
