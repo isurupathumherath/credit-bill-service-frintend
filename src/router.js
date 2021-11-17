859904970717d88c4c9d9b79fb14069225aa1747
IT20206178 <77095664+IT20206178@users.noreply.github.com>
2021-11-17 20:30:44 +0530

    update add customer

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import PrivateRoute from './api/privateRoutes';
 import Login from './components/login/login';
 import App from './App';
 import NotFound from './pages/NotFound';
+import CustomerAdd from './components/CustomerAdd';
 
 const Routes = () => {
     return (
@@ -12,6 +13,7 @@ const Routes = () => {
             <Switch>
                 <PrivateRoute path="/" exact component={App} />
                 <Route path="/login" exact component={Login} />
+                <Route path="/AddCus" exact component={CustomerAdd} />
                 <Route path='/404' component={NotFound} />
                 <Redirect from='*' to='/404' />
             </Switch>
